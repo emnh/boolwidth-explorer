921dccf45b0f40342bba247adb1322d200363136
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2014-11-02 21:57:54 +0100

    Windows compatibility

---
+++ Cakefile.ps1
@@ -0,0 +1,16 @@
+node node_modules\bower\bin\bower install
+cp static\coffee\*.coffee static\coffee.out\
+if (!(test-path static\coffee.out\workers)) { mkdir static\coffee.out\workers }
+cp static\coffee\workers\* static\coffee.out\workers\
+gci static/coffee.out/*.coffee | % {
+  node node_modules\iced-coffee-script\bin\coffee -c -m -o static/coffee.out/ $_.FullName
+}
+gci static\coffee.out\workers\*.coffee | % {
+  node node_modules\iced-coffee-script\bin\coffee -c -m -o static\coffee.out\workers\ $_.FullName
+}
+if (test-path static\coffee.out\bundle.map) { rm -force static\coffee.out\bundle.map }
+if (test-path static\coffee.out\bundle.js) { rm -force static\coffee.out\bundle.js }
+node node_modules\mapcat\bin\mapcat (gci static/coffee.out/*.map | % { $_.FullName }) -m static/coffee.out/bundle.map -j static/coffee.out/bundle.js
+gci static\*.jade | % {
+  node node_modules\jade\bin\jade -P -o static\ $_.FullName
+}
